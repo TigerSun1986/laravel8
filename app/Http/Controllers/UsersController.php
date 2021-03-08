2361b6caa58303cdc777699c0ff9df898bc6fa44
sun tiger <vagrant@homestead>
2021-03-08 09:26:56 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -15,5 +15,15 @@ class UsersController extends Controller
 	public function show(User $user)
     	{
         	return view('users.show', compact('user'));
-    	}
+	}
+
+	public function store(Request $request)
+	{
+		$this->validate($request, [
+			'name' => 'required|unique:users|max:255',
+			'email' => 'required|email|unique:users|max:255',
+			'passowrd' => 'required|confirmed|min:6'
+		]);
+		return;
+	}
 }
